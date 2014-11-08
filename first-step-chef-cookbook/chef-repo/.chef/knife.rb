4418d17ab634bf72dc514e21c9b5aa2c82a68114
Milton Inostroza <milinostroza@groupon.com>
2014-11-08 15:22:58 -0600

    fix bugs in config files

---
+++ first-step-chef-cookbook/chef-repo/.chef/knife.rb
@@ -0,0 +1,1 @@
+cookbook_path ['/root/chef-master/first-step-chef-cookbook/chef-repo/cookbooks'] 
--- first-step-chef-cookbook/chef-solo/wp-secrets.php
+++
@@ -1,8 +0,0 @@
-define('AUTH_KEY',         '{NYgX2_-@4$0(sPbzbL-mJ|[L{t}+_r-}5;$S<+27{mbJh<CrL*C*4NQvL}$XBH=');
-define('SECURE_AUTH_KEY',  'jrnYN-!@Y:Va+kXj|:M.Gu|-Tf7cI+q+n2}RzaC:h=>4#9-|zKYg=4`MK-)Bk`? ');
-define('LOGGED_IN_KEY',    'W^S(Xdu-p2c=Fk#8*hGq^F7MFH._)p[_vd/[(+J~x-0q5(-2,|Es`xT+X72}B^+*');
-define('NONCE_KEY',        '*[l?l^2-Owq]hN:41JI/E:nDl-[s0#d@*I._Cqv6]t|tJe$~|[&>4z(d[0(s/<6-');
-define('AUTH_SALT',        'j|jAf^lk=4#F3S )P|q3|LO3&*U[p!7#k$PalCjT9I`#Sa;p0K:>!}?%X+G-gPq3');
-define('SECURE_AUTH_SALT', 'Yk<dDQ:Bn+YyzEC-p~;YF?oo|292EH_JH@>0bPV_K4~t3]o`FqW|+Vt=fo6BK!q.');
-define('LOGGED_IN_SALT',   'Zy[~LR@4PYmNTQa!>E=%(5dj&lvtxC.pv~jK>;/haCl~*-5&g)gpzHfN-wN`]3|v');
-define('NONCE_SALT',       '_s&9%b0#<Oy.|tY5PJN-u4%%,!xI|+gmXAS6$4sacb4IH`2n2=`|_;MZa;xJu*7!');
